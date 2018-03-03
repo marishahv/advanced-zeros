c4c3dec1953aab35b946c4ac90d6f99980db8446
Marina <35458264+marishahv@users.noreply.github.com>
2018-03-03 15:42:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(number, base) {  
+  var primeFactor, power, countOfprimeFactors, numberOfzeros, k;         
+  numberOfzeros = number;        
+ 
+  for (primeFactor = 2; primeFactor <= base; primeFactor++)
+  {
+     if (base % primeFactor == 0)
+     {   
+        power = 0;  
+        while (base % primeFactor== 0)
+        {
+           power++;
+           base /= primeFactor;
+        }
+        countOfprimeFactors = 0;
+        
+        k = number;                     
+        while (k / primeFactor > 0)
+        {
+           countOfprimeFactors += Math.floor(k / primeFactor);
+           k /= primeFactor;
+        }
+        numberOfzeros = Math.floor(Math.min(numberOfzeros, countOfprimeFactors / power));
+     }
+  }
+  return numberOfzeros;
 }
